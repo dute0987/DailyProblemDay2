eddacba3f36897afd0d8ef3db8f7fa1e80b14661
dute0987 <dute0987@gmail.com>
2022-02-17 15:47:05 +0530

    adding day2 dailyproblem

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PrintUsername
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hello world")
+
+            //int val = 34;
+
+            //double val1 = 3.4;
+
+            //bool val2 = false;
+
+            //char val3 = 'a';
+
+            //string val4 = "Bridgelabz";
+
+            //Console.WriteLine(val);
+
+            //Console.WriteLine(val1);
+
+            //Console.WriteLine(val2);
+
+            //Console.WriteLine(val3);
+
+            //Console.WriteLine(val4);
+
+            //Console.WriteLine("the interger value is ="+ val);
+
+            Console.WriteLine("please Enter your Name");
+
+            string UserName = Console.ReadLine();
+
+            if (UserName.Length>= 3)
+            {
+                Console.WriteLine("Hello " + UserName + ", How are you?");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+
+
+            
+        }
+    }
+}
